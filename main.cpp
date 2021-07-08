9fb87fdf8b61eda680fd1515307fc94825422e8e
Aashish Adhikari <adhikariaashish12@gmail.com>
2021-07-08 13:27:57 +0545

    File structure for project created

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "include/Chess.h"
+#include "include/Checkers.h"
+#include "include/Buddhichal.h"
+
+int main()
+{
+
+  Chess chess;
+  Checkers checkers;
+  Buddhichal buddhichal;
+
+  //chess.run();
+  //checkers.run();
+  //buddhichal.run();
+
+}
